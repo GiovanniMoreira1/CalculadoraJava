f9511c57e762ab76ed67a14ef2e2e671d85c769d
Giovanni Antonio Moreira <giovannimoreira131@gmail.com>
2024-10-01 17:06:11 -0300

    Add files via upload

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,10 @@
+package calculadora;
+import view.Janela;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Janela j = new Janela();
+        j.setVisible(true);
+    }
+    
+}
